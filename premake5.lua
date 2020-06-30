3d74145a04233698a0e947042170d94008b2aa9a
Tobias Brandner <tobias.brandner@stud-mail.uni-wuerzburg.de>
2020-06-30 14:26:38 +0200

    added premake and Generate Porject .bat

---
+++ premake5.lua
@@ -0,0 +1,107 @@
+workspace "Eternal"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Eternal"
+	location "Eternal"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"ET_PLATFORM_WINDOWS",
+			"ET_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "ET_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "ET_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "ET_DIST"
+		optimize "On"
+
+-------------------------------------------------------------------------------------------
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Eternal/vendor/spdlog/include",
+		"Eternal/src"
+	}
+
+	links
+	{
+		"Eternal"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"ET_PLATFORM_WINDOWS",
+		}
+
+	filter "configurations:Debug"
+		defines "ET_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "ET_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "ET_DIST"
+		optimize "On"
