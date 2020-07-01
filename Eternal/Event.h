10233074b29e9447751373b6f3a22ff09e55390e
Tobias Brandner <tobias.brandner@stud-mail.uni-wuerzburg.de>
2020-07-01 11:44:08 +0200

    init basic event system structure

---
+++ Eternal/Event.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "src/Eternal/Core.h"
+
+#include <string>
+#include <functional>
+
+namespace Eternal {
+
+	/*
+		Events for now are blocking, meaning when a event is triggered it
+		has to be dealt right away!
+		Todo: buffer events!
+	*/
+
+	enum class EventType
+	{
+		None = 0,
+		WindowClose, WindowResize, WindowFocus, WindowLostFocus, WindowMoved,
+		AppTick, AppUpdate, AppRender,
+		KeyPressed, KeyReleased,
+		MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled
+	};
+
+	enum EventCategory
+	{
+		None = 0,
+		EventCategoryApplication = BIT(0),
+		EventCategoryInput       = BIT(1),
+		EventCategoryKeyboard    = BIT(2),
+		EventCategoryMouse       = BIT(3),
+		EventCategoryMouseButton = BIT(4),
+	};
+
+
+}
--- Eternal/src/Eternal/Core.h
+++ Eternal/src/Eternal/Core.h
@@ -5,8 +5,9 @@
 		#define ETERNAL_API __declspec(dllexport)
 	#else
 		#define ETERNAL_API __declspec(dllimport)
-
 	#endif
 #else
 	#error Eternal is only for Windows!
 #endif
+
+#define BIT(x) (1 << x)
