83998d3528971d7755797fa99404ca24fe5af909
Tobias Brandner <tobias.brandner@stud-mail.uni-wuerzburg.de>
2020-07-03 11:29:31 +0200

    add EventDispatcher class

--- Eternal/src/Eternal/Events/Event.h
+++ Eternal/src/Eternal/Events/Event.h
@@ -57,7 +57,30 @@ namespace Eternal {
 		bool m_Handled = false;
 	};
 
-	class ETERNAL_API EventDispatcher {
+	class ETERNAL_API EventDispatcher 
+	{
+		template<typename T>
+		using EventFn = std::function<bool(T&)>;
+	public:
+		EventDispatcher(Event& event)
+			: m_Event(event) {}
 
+		template<typename T>
+		bool Dispatch(EventFn<T> func)
+		{
+			if (m_Event.GetEventType() == T::GetStaticType())
+			{
+				m_Event.m_Handled = func(*(T*)&m_Event);
+				return true;
+			}
+			return false;
+		}
+	private:
+		Event& m_Event;
 	};
+
+	inline std::ostream& operator<<(std::ostream& os, const Event& e)
+	{
+		return os << e.ToString();
+	}
 }
