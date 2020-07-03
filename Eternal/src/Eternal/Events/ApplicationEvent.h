c8f37dad67c570b4cbd126ea86fad8290103892f
Tobias Brandner <tobias.brandner@stud-mail.uni-wuerzburg.de>
2020-07-03 11:29:03 +0200

    add Application Events like Window Resize and Window Close

--- Eternal/src/Eternal/Events/ApplicationEvent.h
+++ Eternal/src/Eternal/Events/ApplicationEvent.h
@@ -1,1 +1,71 @@
 #pragma once
+
+#include "Event.h"
+
+#include <sstream>
+
+namespace Eternal {
+
+	class ETERNAL_API WindowResizeEvent : public Event
+	{
+	public:
+		WindowResizeEvent(unsigned int width, unsigned int height)
+			: m_Width(width), m_Height(height) {}
+
+		inline unsigned int GetWidth() const { return m_Width; }
+		inline unsigned int GetHeight() const { return m_Height; }
+
+		std::string ToString() const override
+		{
+			std::stringstream sstring;
+			sstring << "WindowResizeEvent: " << m_Width << ", " << m_Height;
+			return sstring.str();
+		}
+
+		EVENT_CLASS_TYPE(WindowResize)
+		EVENT_CLASS_CATEGORY(EventCategoryApplication)
+	private:
+		unsigned int m_Width, m_Height;
+	};
+
+	class ETERNAL_API WindowCloseEvent : public Event
+	{
+	public:
+		WindowCloseEvent() {}
+
+		EVENT_CLASS_TYPE(WindowClose)
+		EVENT_CLASS_CATEGORY(EventCategoryApplication)
+	};
+
+	/*
+	---------------------------------------------------------------------------------------------------------------------------------------------------------
+		next three Events maybe unnecassary, since they will be present in every Application, and might not be extra called as an event..
+	*/
+	class ETERNAL_API AppTickEvent : public Event
+	{
+	public:
+		AppTickEvent() {}
+
+		EVENT_CLASS_TYPE(AppTick)
+		EVENT_CLASS_CATEGORY(EventCategoryApplication)
+	};
+
+	class ETERNAL_API AppUpdateEvent : public Event
+	{
+	public:
+		AppUpdateEvent() {}
+
+		EVENT_CLASS_TYPE(AppUpdate)
+		EVENT_CLASS_CATEGORY(EventCategoryApplication)
+	};
+
+	class ETERNAL_API AppRenderEvent : public Event
+	{
+	public:
+		AppRenderEvent() {}
+
+		EVENT_CLASS_TYPE(AppRender)
+		EVENT_CLASS_CATEGORY(EventCategoryApplication)
+	};
+
+}
