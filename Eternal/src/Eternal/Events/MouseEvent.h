ba854bc87a4dbdde4255280eaaae62ef31a55094
Tobias Brandner <tobias.brandner@stud-mail.uni-wuerzburg.de>
2020-07-03 10:58:45 +0200

    added Mouse Events

--- Eternal/src/Eternal/Events/MouseEvent.h
+++ Eternal/src/Eternal/Events/MouseEvent.h
@@ -1,1 +1,97 @@
 #pragma once
+
+#include "Event.h"
+
+#include <sstream>
+
+namespace Eternal{
+
+	class ETERNAL_API MouseMovedEvent : public Event
+	{
+	public:
+		MouseMovedEvent(float x, float y)
+			: m_MouseX(x), m_MouseY(y) {}
+
+		inline float GetX() const { return m_MouseX; }
+		inline float GetY() const { return m_MouseY; }
+
+		std::string ToString() const override
+		{
+			std::stringstream sstring;
+			sstring << "MouseMovedEvent: " << m_MouseX << ", " << m_MouseY;
+			return sstring.str();
+		}
+
+		EVENT_CLASS_TYPE(MouseMoved)
+		EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput)
+	private:
+		float m_MouseX, m_MouseY;
+	};
+
+	class ETERNAL_API MouseScrolledEvent : public Event
+	{
+	public:
+		MouseScrolledEvent(float xOffset, float yOffset) 
+			: m_XOffset(xOffset), m_YOffset(yOffset) {}
+
+		inline float GetXOffset() const { return m_XOffset; }
+		inline float GetYOffset() const { return m_YOffset; }
+
+		std::string ToString() const override
+		{
+			std::stringstream sstring;
+			sstring << "MouseScrolledEvent: " << m_XOffset << ", " << m_YOffset;
+			return sstring.str();
+		}
+
+		EVENT_CLASS_TYPE(MouseScrolled)
+		EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput)
+	private:
+		float m_XOffset, m_YOffset;
+	};
+
+	class ETERNAL_API MouseButtonEvent : public Event
+	{
+	public:
+		inline int GetMouseButton() const { return m_Button; }
+
+		EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput)
+	protected:
+		MouseButtonEvent(int button)
+			: m_Button(button) {}
+
+		int m_Button;
+	};
+
+	class ETERNAL_API MouseButtonPressedEvent : public MouseButtonEvent
+	{
+	public:
+		MouseButtonPressedEvent(int button)
+			: MouseButtonEvent(button) {}
+
+		std::string ToString() const override
+		{
+			std::stringstream sstring;
+			sstring << "MouseButtonPressedEvent: " << m_Button;
+			return sstring.str();
+		}
+
+		EVENT_CLASS_TYPE(MouseButtonPressed)
+	};
+
+	class ETERNAL_API MouseButtonReleasedEvent : public MouseButtonEvent
+	{
+	public:
+		MouseButtonReleasedEvent(int button)
+			: MouseButtonEvent(button) {}
+
+		std::string ToString() const override
+		{
+			std::stringstream sstring;
+			sstring << "MouseButtonReleasedEvent: " << m_Button;
+			return sstring.str();
+		}
+
+		EVENT_CLASS_TYPE(MouseButtonReleased)
+	};
+}
