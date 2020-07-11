b2c0aac583c5b596be9ae2af0af7341cfcf36648
Tobias Brandner <tobias.brandner@stud-mail.uni-wuerzburg.de>
2020-07-11 16:34:41 +0200

    changed premake file to move dll after Project is created

--- premake5.lua
+++ premake5.lua
@@ -127,7 +127,7 @@ project "Sandbox"
 
 		postbuildcommands
 		{
-			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+			("{COPY} ../bin/" .. outputdir .. "/Eternal/Eternal.dll" .. "../bin/" .. outputdir .. "/%{prj.name}")
 		}
 
 	filter "configurations:Debug"
