418717d1b617602dcf726e5109f769eaf5bf85a2
Tobias Brandner <tobias.brandner@stud-mail.uni-wuerzburg.de>
2020-07-19 11:20:27 +0200

    add basic shader example

--- Eternal/src/Eternal/Application.h
+++ Eternal/src/Eternal/Application.h
@@ -9,6 +9,8 @@
 
 #include "Eternal/ImGui/ImGuiLayer.h"
 
+#include "Eternal/Renderer/Shader.h"
+
 namespace Eternal {
 
 	class ETERNAL_API Application
@@ -34,6 +36,7 @@ namespace Eternal {
 		LayerStack m_LayerStack;
 
 		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		std::unique_ptr<Shader> m_Shader;
 	private:
 		static Application* s_Instance;
 	};
